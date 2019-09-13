97e6a1af817f3dc4bfd18b0b36f8b9bc82d32dd0
nick stern <nicholas.stern2@gmail.com>
2019-09-13 17:42:22 +0800

    social links on side/top. Navbar transparent=> not on scroll. nav disaapears on scroll down and back on scroll up

---
+++ app/javascript/plugins/navbar.js
@@ -0,0 +1,20 @@
+
+//************  TRANSPARENT NAVBAR **************
+
+const initUpdateNavbarOnScroll = () => {
+  const navbar = document.querySelector('.navbar-lewagon');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight - 600) {
+        navbar.classList.add('navbar-lewagon-white');
+      } else {
+        navbar.classList.remove('navbar-lewagon-white');
+      }
+    });
+  }
+}
+
+export { initUpdateNavbarOnScroll };
+
+// ************ END **************
+
