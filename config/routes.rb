d8ee2e5dc1420bf64189386decfe65457ab243ae
nick stern <nicholas.stern2@gmail.com>
2019-09-13 20:46:10 +0800

    router to education

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
+  get "education", to: 'pages#education', as: :education
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
